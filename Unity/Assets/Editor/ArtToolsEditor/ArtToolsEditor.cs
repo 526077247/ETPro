d3a45907ad4ba4b82ca054d4f3d4702dd0e57cb5
Tailor <526077247@qq.com>
2023-05-04 12:19:47 +0800

    整理文件

--- Unity/Assets/Editor/ArtToolsEditor/ArtToolsEditor.cs
+++ Unity/Assets/Editor/ArtToolsEditor/ArtToolsEditor.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEditor;
+using UnityEngine;
 
 namespace ET
 {
@@ -46,6 +47,30 @@ namespace ET
         public static void CreateArtFont()
         {
             ArtistFont.BatchCreateArtistFont();
+        }
+        
+        [MenuItem("Tools/ArtTools/搜索或批量替换Sprite", false, 503)]
+        public static void ReplaceImage()
+        {
+            Rect _rect = new Rect(0, 0, 900, 600);
+            ReplaceImage window = EditorWindow.GetWindowWithRect<ReplaceImage>(_rect, true, "搜索或批量替换Sprite");
+            window.Show();
+        }
+
+        [MenuItem("Tools/ArtTools/查找未使用的图片", false, 503)]
+        public static void CheckUnUseImage()
+        {
+            Rect _rect = new Rect(0, 0, 900, 600);
+            CheckUnuseImage window = EditorWindow.GetWindowWithRect<CheckUnuseImage>(_rect, true, "查找未使用的图片");
+            window.Show();
+        }
+
+        [MenuItem("Tools/ArtTools/检查丢失image", false, 504)]
+        public static void CheckLossImage()
+        {
+            Rect _rect = new Rect(0, 0, 900, 600);
+            CheckEmptyImage window = EditorWindow.GetWindowWithRect<CheckEmptyImage>(_rect, true, "检查预设丢失image");
+            window.Show();
         }
     }
 }
